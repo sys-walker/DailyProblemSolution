e88b1bb11645f5e18bf17b0e9d3312904daee74d
sys-walker <56586435+sys-walker@users.noreply.github.com>
2020-08-30 14:47:50 +0200

    Update number_of_ways_to_climb_stairs.py

--- Solutions/number_of_ways_to_climb_stairs.py
+++ Solutions/number_of_ways_to_climb_stairs.py
@@ -1,4 +1,20 @@
 #!/bin/python3
+class queue:
+    def __init__(self):
+        self.__theStack = []
+
+    def enqueue(self, val):
+        self.__theStack.append(val)
+
+    def dequeue(self):
+        self.__theStack.pop(0)
+
+    def rear(self):
+        return self.__theStack[0]
+
+    def front(self):
+        return self.__theStack[len(self.__theStack) - 1]
+
 
 def staircase(n):
     # Fill this in.
@@ -10,8 +26,31 @@ def staircase(n):
         return staircase(n - 2) + staircase(n - 1)
 
 
+def iterative_staircase(n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    else:
+        q = queue()
+        q.enqueue(1)
+        q.enqueue(2)
+        acc, i = 0, 3
+        while i < n + 1:
+            acc = q.front() + q.rear()
+            q.enqueue(acc)
+            q.dequeue()
+
+            i += 1
+        return acc
+
+
 if __name__ == '__main__':
     print(staircase(4))
     # 5
     print(staircase(5))
+    # 8
+    print(iterative_staircase(4))
+    # 5
+    print(iterative_staircase(5))
     # 8
